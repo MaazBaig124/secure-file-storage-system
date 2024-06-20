f4f9850b5ea2c2f6ca12387a95b4cefdb983eb43
Maaz Baig <maazbaig@MaazDell>
2024-06-19 22:47:11 -0400

    changed index.html to generate the token and allow uploads

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, request, render_template, jsonify
+from flask import Blueprint, request, render_template, jsonify, current_app
 from .auth import token_required
 from .s3 import upload_file_to_s3
 from .encryption import encrypt_file
+import jwt
+import datetime
 
 bp = Blueprint('routes', __name__)
 
@@ -20,3 +22,11 @@ def upload_file():
     s3_key = upload_file_to_s3(encrypted_file, file.filename)
     
     return jsonify({"message": "File uploaded successfully", "s3_key": s3_key})
+
+@bp.route('/generate_token', methods=['GET'])
+def generate_token():
+    token = jwt.encode({
+        'user': 'test_user',
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    }, current_app.config['SECRET_KEY'], algorithm="HS256")
+    return jsonify({'token': token})
